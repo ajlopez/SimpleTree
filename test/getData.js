b2f6193f79924310c3edb88ad587946b7012801d
ajlopez <webmaster@ajlopez.com>
2012-07-02 21:45:37 -0300

    create second level node, with test

--- test/getData.js
+++ test/getData.js
@@ -19,3 +19,11 @@ result = tree.getData('/node1');
 assert.ok(result);
 assert.ok(result.data);
 assert.equal(result.data, 2);
+
+tree.createNode('/node1/subnode2', { data: 3 });
+
+result = tree.getData('/node1/subnode2');
+assert.ok(result);
+assert.ok(result.data);
+assert.equal(result.data, 3);
+
